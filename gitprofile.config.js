575a3682092c5cd7564424f67c16c550bce4d079
Sagar Goswami <88379870+sagargoswami2001@users.noreply.github.com>
2023-03-16 13:15:24 +0530

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -2,111 +2,153 @@
 
 const config = {
   github: {
-    username: 'arifszn', // Your GitHub org/user name. (Required)
+    username: 'sagargoswami2001', // Your GitHub org/user name. (Required)
     sortBy: 'stars', // stars | updated
     limit: 10, // How many projects to display.
     exclude: {
       forks: false, // Forked projects will not be displayed if set to true.
-      projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
+      projects: ['sagargoswami2001', 'Python-Conditional-Statement-Programs', 'Simple-Web-Browser-Using-Python', 'Java-Lab-File-Programs', 'Love-Calculator-Using-Python', 'Python-Basic-Programs-2', 'School-Management-System-Using-Python', 'Credit-Card-Validator', 'Login-Form-Validation', 'Team-Section-Page', 'About-Us-Site-Page', 'Python-Turtle-Codes', 'C-Lab-File-Programs', 'Sign-Up-and-Login-Page', 'C-Programs', 'Calendar-in-C', 'Snake-Game', 'Cpp-Lab-File-Programs', 'Data-Structure-Lab-File-Programs', 'Rolling-Dice', 'Notification-Reminder', 'Tindog-Site', 'Java-Programs-For-Practice', 'CV-Sample-Using-HTML-And-CSS-Dark-Theme', 'CV-Sample-Using-HTML-And-CSS', 'Basic-CV-Sample-Using-HTML-And-CSS', 'Mouse-Using-Your-Eyes', 'Basic-Billing-Machine', 'Bitcoin-Mining', 'Social-Media-Button-Hover-Effect', '3D-Flip-Card', 'Simple-Blockchain', 'ATM-Machine-in-C', 'Java-Pattern-Programs', 'Python-Arrays-Programs', 'Dark-Mode-Toggle', 'Basic-Flipkart-Front-Page-Clone', 'Python-Basic-Programs-1', '100-Practice-Problems-in-Cpp', 'CGMA-Lab-File-Programs', 'Setup-Graphics.h-for-Visual-Studio-Code', 'How-Does-Bitcoin-Mining-Work', 'Snake-Water-Gun', 'Funny-Hack', 'Command-Line-Interface'], // These projects will not be displayed. example: ['my-project1', 'my-project2']
     },
   },
   social: {
-    linkedin: 'ariful-alam',
-    twitter: 'arif_szn',
-    mastodon: 'arifszn@mastodon.social',
+    linkedin: 'sagargoswami2001',
+    twitter: 'Sagar_Goswami27',
+    mastodon: '',
     facebook: '',
-    instagram: '',
+    instagram: 'sagar_goswami.2001',
     dribbble: '',
     behance: '',
-    medium: 'arifszn',
-    dev: 'arifszn',
+    medium: '',
+    dev: '',
     stackoverflow: '', // format: userid/username
     skype: '',
-    telegram: '',
-    website: 'https://arifszn.com',
-    phone: '',
-    email: 'arifulalamszn@gmail.com',
+    telegram: 'sagargoswami2001',
+    website: 'sagargoswami2001.github.io',
+    phone: '9837876110',
+    email: 'sagargoswami7417@gmail.com',
   },
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      '', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
-    'PHP',
-    'Laravel',
-    'JavaScript',
-    'React.js',
-    'Node.js',
-    'Nest.js',
-    'MySQL',
-    'PostgreSQL',
     'Git',
-    'Docker',
-    'PHPUnit',
+    'Github',
+    'Github Actions',
+    'C',
+    'C++',
+    'C#',
+    'Python',
+    'Blockchain',
+    'HTML',
     'CSS',
-    'Antd',
-    'Tailwind',
+    'JavaScript',
+    'Jquery',
+    'Bootstrap',
+    'PHP',
+    'Markdown',
+    'Hugo',
+    'Java',
+    'Vim',
+    'Linux',
+    'Windows',
+    'Bash',
+    'MySQL',
+    'SQLite',
+    'Photoshop',
+    'SVG',
+    'Office-Suite',
   ],
   experiences: [
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'September 2021',
-      to: 'Present',
-      companyLink: 'https://example.com',
+      company: 'Jumpstart Blockchain',
+      position: 'Technical Content Writer',
+      from: 'March 2022',
+      to: 'April 2022',
+      companyLink: 'https://www.jumpstartblockchain.com/members/sagar-goswami',
     },
-    {
+   /* {
       company: 'Company Name',
       position: 'Position',
       from: 'July 2019',
       to: 'August 2021',
       companyLink: 'https://example.com',
+    }, */
+  ],
+  certifications: [
+    {
+      name: 'Blockchain Basics',
+      body: 'University at Buffalo',
+      year: 'Mar 2023',
+      link: 'https://www.coursera.org/account/accomplishments/verify/97RXMVMFXLEN'
+    },
+    {
+      name: 'Crash Course on Python',
+      body: 'Google',
+      year: 'Feb 2023',
+      link: 'https://www.coursera.org/account/accomplishments/verify/Z43X5WJXR2UU'
+    },
+    {
+      name: 'Introduction to Cybersecurity',
+      body: 'Cisco',
+      year: 'March 2023',
+      link: 'https://www.credly.com/badges/c8caa3b7-9ff3-4d4d-82c0-595bf8cc8353/linked_in_profile'
+    },
+    {
+      name: 'Version Control',
+      body: 'Meta',
+      year: 'Oct 2022',
+      link: 'https://www.coursera.org/account/accomplishments/verify/RP59Q98BPMWV'
+    },
+    {
+      name: 'Sales and CRM Overview',
+      body: 'Salesforce',
+      year: 'Jan 2023',
+      link: 'https://www.coursera.org/account/accomplishments/verify/VPRYVB33AQPF'
+    },
+    {
+      name: 'PCAP: Programming Essentials in Python',
+      body: 'OpenEDG Python Institute',
+      year: 'Mar 2022',
+      link: 'https://id.cisco.com'
     },
   ],
-  /* certifications: [
-    {
-      name: 'Lorem ipsum',
-      body: 'Lorem ipsum dolor sit amet',
-      year: 'March 2022',
-      link: 'https://example.com'
-    },
-  ], */
   education: [
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2015',
-      to: '2019',
+      institution: 'Integrated Academy Of Management and Technology',
+      degree: 'Bachelor of Computer Application',
+      from: '2020',
+      to: '2023',
     },
-    {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2012',
-      to: '2014',
-    },
+   /* {
+      institution: 'Ch. Tara Chand Janta Inter College',
+      degree: 'Intermediate',
+      from: '2019',
+      to: '2020',
+    }, */
   ],
 
   // To hide the `My Projects` section, keep it empty.
   externalProjects: [
     {
-      title: 'Project Name',
+      title: 'Dressingnity',
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-      imageUrl: 'https://via.placeholder.com/250x250',
-      link: 'https://example.com',
+        'A Dummy E-Commerce website',
+      imageUrl: '',
+      link: 'https://dressingnity.rf.gd/',
     },
     {
-      title: 'Project Name',
+      title: 'School Management System',
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-      imageUrl: 'https://via.placeholder.com/250x250',
-      link: 'https://example.com',
+        'GUI Based School Management System Using the Tkinter, SQLite3, & TkCalender Libraries',
+      imageUrl: '',
+      link: 'https://github.com/sagargoswami2001/School-Management-System-Using-Python',
     },
   ],
   // Display blog posts from your medium or dev account. (Optional)
   blog: {
-    source: 'dev', // medium | dev
-    username: 'arifszn', // to hide blog section, keep it empty
+    source: '', // medium | dev
+    username: '', // to hide blog section, keep it empty
     limit: 2, // How many posts to display. Max is 10.
   },
   googleAnalytics: {
@@ -118,7 +160,7 @@ const config = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'winter',
+    defaultTheme: 'Luxury',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
@@ -177,12 +219,12 @@ const config = {
     },
   },
 
-  // Optional Footer. Supports plain text or HTML.
-  footer: `Made with <a 
-      class="text-primary" href="https://github.com/arifszn/gitprofile"
-      target="_blank"
-      rel="noreferrer"
-    >GitProfile</a> and ❤️`,
+// Optional Footer. Supports plain text or HTML.
+//  footer: `Made with <a 
+//      class="text-primary" href="https://github.com/arifszn/gitprofile"
+//      target="_blank"
+//      rel="noreferrer"
+//    >GitProfile</a> and ❤️`,
 };
 
 export default config;
